b10b93b3d09e529f6f798436b2dc490ee4af7d06
sadewathea <yana.nugraha@gmail.com>
2019-05-17 08:09:37 +0700

    Update koneksi.php

--- utama/koneksi.php
+++ utama/koneksi.php
@@ -9,13 +9,22 @@ mysql_select_db($db,$sambung);
 **/
     $host = "hexawebappserver.database.windows.net";
     $user = "hexasoft";
-    $pass = "iqbal2410";
+    $pass = "Iqbal2410";
     $db = "hexawebapp";
-
+/**
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
     }
+**/
+try {
+    $conn = new PDO("sqlsrv:server = tcp:hexawebappserver.database.windows.net,1433; Database = hexawebapp", "hexasoft", "Iqbal2410");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 ?>
