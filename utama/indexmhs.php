6413ac8106792549c3537da2e07b78ace82b7feb
sadewathea <yana.nugraha@gmail.com>
2019-05-17 08:18:33 +0700

    Update indexmhs.php

--- utama/indexmhs.php
+++ utama/indexmhs.php
@@ -15,12 +15,14 @@
     $pass = "Iqbal2410";
     $db = "hexawebapp";
 
-    try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    } catch(Exception $e) {
-        echo "Failed: " . $e;
-    }
+try {
+    $conn = new PDO("sqlsrv:server = tcp:hexawebappserver.database.windows.net,1433; Database = hexawebapp", "hexasoft", "Iqbal2410");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 try {
             $sql_select = "SELECT * FROM mahasiswa";
             $stmt = $conn->query($sql_select);
